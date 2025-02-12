306633c38d51be45579f94c68624a24325bcd8f5
brayan-otieno <briandan2058@gmail.com>
2025-02-13 02:55:37 +0300

    Add Django Admin Interface

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',  # Ensure this is added
+    'bookshelf', 
 ]
+
 
 
 MIDDLEWARE = [
--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,11 @@
+# bookshelf/admin.py
 from django.contrib import admin
+from .models import Book  # Ensure this imports your Book model
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Customize the display
+    search_fields = ('title', 'author')  # Make title and author searchable
+    list_filter = ('publication_year',)  # Filter by publication year
+
+# Register the model with the admin site
+admin.site.register(Book, BookAdmin)
