434220e3f8b0cfe67d4feb09c81f9dbfd7025653
Brayan Otieno <160001650+brayan-otieno@users.noreply.github.com>
2025-02-26 11:12:56 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,94 +1,24 @@
 from django.shortcuts import render
 from .models import Book
-from .models import Library
-from django.views.generic.detail import DetailView
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.http import HttpResponseForbidden
-from .models import UserProfile
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
 
 def list_books(request):
-	books = Book.objects.all()
-	return render(request, "relationship_app/list_books.html", {'books': books})
+    # Fetch all books from the database
+    books = Book.objects.all()
+    
+    # Render the list_books.html template, passing the books
+    return render(request, 'list_books.html', {'books': books})
+
+from django.views.generic import DetailView
+from .models import Library
 
 class LibraryDetailView(DetailView):
-	model = Library
-	template_name = 'relationship_app/library_detail.html'
-	context_object_name = 'library'
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  
-            return redirect('login')  
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
-
-class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
-
-class CustomLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
-	
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
-
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
-@login_required
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-@login_required
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-@login_required
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
-
-@permission_required('relationship_app.can_add_book', raise_exception=True)
-def add_book(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        author_id = request.POST.get('author')
-        publication_year = request.POST.get('publication_year')
-
-        book = Book(title=title, author_id=author_id, publication_year=publication_year)
-        book.save()
-        return redirect('book_list')  
-    return render(request, 'relationship_app/add_book.html')
-
-@permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-
-    if request.method == 'POST':
-        book.title = request.POST.get('title')
-        book.author_id = request.POST.get('author')
-        book.publication_year = request.POST.get('publication_year')
-        book.save()
-        return redirect('book_list')  
-    return render(request, 'relationship_app/edit_book.html', {'book': book})
-
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book_list')  
-
-    return render(request, 'relationship_app/delete_book.html', {'book': book})
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+    
+    # Optionally, you can override get_context_data to add extra context.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Adding the list of books available in this library
+        context['books'] = self.object.books.all()  # Assuming the Library model has a related name for books
+        return context
