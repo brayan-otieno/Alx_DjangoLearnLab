c8d562dd050b6f302ed19c5d47e7d29effa5f8ab
Brayan Otieno <160001650+brayan-otieno@users.noreply.github.com>
2025-02-26 11:42:05 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,51 +1,32 @@
-from .models import Book, Library
+"""
+URL configuration for LibraryProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
-from . import views
-from .views import LibraryDetailView
-from .views import list_books
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
-    path('book/', views.viewmodel,name = 'bookd' ),
-    path('library/', LibraryDetailView.as_view(), name = 'list' ),
-    path('register/', views.register),
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
-    
-    path('admin/', views.admin_view, name='admin_view'),
-    path('librarian/', views.librarian_view, name='librarian_view'),
-    path('member/', views.member_view, name='member_view'),
+    path('admin/', admin.site.urls),
 ]
 
+# LibraryProject/urls.py
 
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("books/add/", views.add_book, name="add_book"),
-    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
-    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
-    # Other URLs for the app
-]
-
-# relationship_app/urls.py
-from django.urls import path
-from .views import add_book, edit_book
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('add_book/', add_book, name='add_book'),
-    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
-    # other URL patterns
-]
-
-# relationship_app/urls.py
-from django.urls import path
-from .views import add_book, edit_book, delete_book
-
-urlpatterns = [
-    path('add_book/', add_book, name='add_book'),
-    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
-    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
-    # other URL patterns
+    path('admin/', admin.site.urls),
+    path('relationship_app/', include('relationship_app.urls')),  # Include relationship_app URLs
 ]
