1751eb2b0e72097dfa154ad6041126120b3e2fde
Brayan Otieno <160001650+brayan-otieno@users.noreply.github.com>
2025-03-06 11:01:46 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,13 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated  # Permission to allow only authenticated users
-from rest_framework.authentication import TokenAuthentication  # Token Authentication
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # Retrieve all books from the database
     serializer_class = BookSerializer  # Serialize the books using BookSerializer
-    
-    # Add authentication and permission classes
-    authentication_classes = [TokenAuthentication]  # Use TokenAuthentication for authenticated requests
-    permission_classes = [IsAuthenticated]  # Ensure only authenticated users can access this view
-
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access this view
